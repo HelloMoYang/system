0e68125c42817209121cfec0dffda2e3c2ef7baa
yangmo <374577605@qq.com>
2017-12-25 18:12:48 +0800

    UsernamePasswordToken

--- src/main/java/com/my/system/Config/ShiroConfiguration.java
+++ src/main/java/com/my/system/Config/ShiroConfiguration.java
@@ -1,48 +1,49 @@
-//package com.my.system.Config;
-//
-//import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
-//import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//import java.util.LinkedHashMap;
-//import java.util.Map;
-//
-//@Configuration
-//public class ShiroConfiguration {
-//
-//    /**
-//     * ShiroFilterFactoryBean 处理拦截资源文件问题。
-//     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，因为在
-//     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
-//     */
-//    @Bean
-//    public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager){
-//        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
-//        shiroFilterFactoryBean.setSecurityManager(securityManager);
-//        Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
-//        filterChainDefinitionMap.put("/logout","logout");
-//        filterChainDefinitionMap.put("/**","authc");
-//
-//        shiroFilterFactoryBean.setLoginUrl("/login");
-//        shiroFilterFactoryBean.setSuccessUrl("/loginSuccess");
-//        //  未授权界面
-//        //  shiroFilterFactoryBean.setUnauthorizedUrl("/403");
-//        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
-//        return shiroFilterFactoryBean;
-//    }
-//
-//    @Bean
-//    public DefaultWebSecurityManager securityManager(){
-//        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
-//        //  设置realm
-//        securityManager.setRealm(myShiroRealm());
-//        return securityManager;
-//    }
-//
-//    @Bean
-//    public MyShiroRealm myShiroRealm(){
-//        MyShiroRealm myShiroRealm = new MyShiroRealm();
-//        return myShiroRealm;
-//    }
-//}
+package com.my.system.Config;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfiguration {
+
+    /**
+     * ShiroFilterFactoryBean 处理拦截资源文件问题。
+     * 注意：单独一个ShiroFilterFactoryBean配置是或报错的，因为在
+     * 初始化ShiroFilterFactoryBean的时候需要注入：SecurityManager
+     */
+    @Bean
+    public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager securityManager){
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        Map<String,String> filterChainDefinitionMap = new LinkedHashMap<String,String>();
+        filterChainDefinitionMap.put("/userLogin","anon");
+        filterChainDefinitionMap.put("/logout","logout");
+        filterChainDefinitionMap.put("/**","authc");
+        //  不管输入怎样的url，都会跳转到此url，然后访问Controller，不设置此项目将默认跳转login页面
+        shiroFilterFactoryBean.setLoginUrl("/");
+        shiroFilterFactoryBean.setSuccessUrl("/loginSuccess");
+        //  未授权界面
+        //  shiroFilterFactoryBean.setUnauthorizedUrl("/403");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+    }
+
+    @Bean
+    public DefaultWebSecurityManager securityManager(){
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        //  设置realm
+        securityManager.setRealm(myShiroRealm());
+        return securityManager;
+    }
+
+    @Bean
+    public MyShiroRealm myShiroRealm(){
+        MyShiroRealm myShiroRealm = new MyShiroRealm();
+        return myShiroRealm;
+    }
+}
--- src/main/java/com/my/system/Controller/UserLoginController.java
+++ src/main/java/com/my/system/Controller/UserLoginController.java
@@ -1,38 +1,40 @@
-//package com.my.system.Controller;
-//
-//import com.my.system.Bean.UserLogin;
-//import org.apache.shiro.SecurityUtils;
-//import org.apache.shiro.authc.AccountException;
-//import org.apache.shiro.authc.UsernamePasswordToken;
-//import org.apache.shiro.subject.Subject;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.servlet.ModelAndView;
-//
-//import javax.servlet.http.HttpSession;
-//
-//
-//@Controller
-//public class UserLoginController {
-//
-//    @PostMapping(value = "/userLogin")
-//    public String login(@RequestParam("username") String username,
-//                        @RequestParam("password") String password, HttpSession session){
-//        //  获取Subject单例对象
-//        Subject currentUser = SecurityUtils.getSubject();
-//        //  使用用户的登录信息创建用户令牌
-//        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
-//        try {
-//            currentUser.login(token);
-//            UserLogin userLogin = (UserLogin) currentUser.getPrincipal();
-//            session.setAttribute("userLogin",userLogin);
-//            return "/loginSuccess";
-//        }catch(AccountException e){
-//            ModelAndView mv = new ModelAndView("error");
-//            mv.addObject("msg","用户名或密码不正确！");
-//            return "/login";
-//        }
-//    }
-//
-//}
+package com.my.system.Controller;
+
+import com.my.system.Bean.UserLogin;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AccountException;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+
+@Controller
+public class UserLoginController {
+
+    @PostMapping(value = "/userLogin")
+    public ModelAndView login(@RequestParam("username") String username,
+                        @RequestParam("password") String password, HttpSession session){
+        //  获取Subject单例对象
+        Subject currentUser = SecurityUtils.getSubject();
+        //  使用用户的登录信息创建用户令牌
+        UsernamePasswordToken token = new UsernamePasswordToken(username,password);
+        try {
+            currentUser.login(token);
+            UserLogin userLogin = (UserLogin) currentUser.getPrincipal();
+            session.setAttribute("userLogin",userLogin);
+            ModelAndView mv = new ModelAndView("loginSuccess");
+            return mv;
+        }catch(AuthenticationException e){
+            ModelAndView mv = new ModelAndView("error");
+            mv.addObject("msg","用户名或密码不正确！");
+            return mv;
+        }
+    }
+
+}
